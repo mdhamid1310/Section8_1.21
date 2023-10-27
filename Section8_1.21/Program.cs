f798c5a3c2aa94180b2b2bf13294c693ccd791be
mdhamid1310 <john@example.com>
2023-10-27 09:52:12 +0530

    Add project files.

---
+++ Section8_1.21/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section8_1._21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+            Console.ReadLine();
+        }
+        public static void runApp()
+        {
+            string[] students = { "Bob", "Alice", "David", "Grace", "Jack", "Henry", "Frank", "Ivy", "Charlie", "Ella" };
+
+            Console.WriteLine("Unsorted Students:");
+            DisplayStudents(students);
+
+            SelectionSort(students);
+
+            Console.WriteLine("\nSorted Students:");
+            DisplayStudents(students);
+        }
+
+        public static void SelectionSort(string[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (string.Compare(arr[j], arr[minIndex]) < 0)
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i)
+                {
+                    // Swap the elements
+                    string temp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = temp;
+                }
+            }
+        }
+
+        public static void DisplayStudents(string[] arr)
+        {
+            foreach (string student in arr)
+            {
+                Console.Write(student + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
